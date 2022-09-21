5fbf41d064b31dcbff1475cc945cf715e6574b1a
chabid492 <chabid492@gmail.com>
2022-09-21 10:38:46 +0500

    integrating bootstrap 5 theme in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { HeaderComponent } from './header/header.component';
-// import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  // {
-  //   path: 'header',
-  //   component: HeaderComponent
-  // },
-  // {
-  //   path: 'footer',
-  //   component: FooterComponent
-  // }
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'category',
+    component: CategoryComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent,
     FooterComponent,
   ],
   imports: [
